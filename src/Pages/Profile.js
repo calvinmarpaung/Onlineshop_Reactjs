3610b8c61b1a9a30e58ebea0e3d00505e93264c6
Calvin Haposan Marpaung <106538712+calvinmarpaung@users.noreply.github.com>
2022-09-06 23:37:36 +0700

    Update Profile.js

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Profile = () =>{
     return(
-        <div>
-            <h1>Profile</h1>
+        <div className = "relative">
+            <div className="absolute top-20 left-20 ">
+                <h1 className="text-8xl">Welcome to Calvin Shop</h1>
+            </div>
         </div>
     );
 } 
