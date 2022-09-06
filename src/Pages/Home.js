009f88b7ec4a47dc6b41ea0d0f0c953e85517feb
Calvin Haposan Marpaung <106538712+calvinmarpaung@users.noreply.github.com>
2022-09-06 23:38:11 +0700

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 
 const Home = () =>{
     return(
-        // <div className='flex flex-row py-2 px-4' >
-        //     <div className="bg-gradient-to-l from-slate-300 to-red-400 w-10 h-10 rounded-lg py-2">
-        //     <div className="bg-gradient-to-l from-slate-300 to-blue-400 w-5 h-5 rounded-lg mx-auto  "/>
-        //     </div>
-        //     <span className="text-3xl text-black-400 ml-2 font-serif ">Calvin</span>
-        // </div>
-        <div></div>
+        <div className="absolute top-20 left-20">
+        <div className="flex flex-row py-2 px-4" >
+            <div className="bg-gradient-to-l from-slate-300 to-red-400 w-10 h-10 rounded-lg py-2">
+            <div className="bg-gradient-to-l from-slate-300 to-blue-400 w-5 h-5 rounded-lg mx-auto  "/>
+            </div>
+            <span className="text-3xl text-black-400 ml-2 font-serif ">Calvin</span>
+        </div>
+        </div>
+        
     );
 } 
 
